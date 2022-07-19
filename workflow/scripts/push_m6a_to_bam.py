5a58c0cf460f0937f6d2ff34d3e7bcec4d44d9e1
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-07-19 12:12:26 -0700

    basic test case

--- workflow/scripts/push_m6a_to_bam.py
+++ workflow/scripts/push_m6a_to_bam.py
@@ -26,9 +26,9 @@ def coordinateConversion_MMTag(sequence, base, modification_coords):
     # i.e. As  on + , Ts on -, we only want the mods that = that base of interest
     masked_coords = np.cumsum(mask)[coords] - 1
     distance = np.diff(masked_coords) - 1
-    MM_coords = ",".join([coords[0].astype(str)] + list(distance.astype(str)))
-
-    return MM_coords
+    if coords.shape[0] < 1:
+        return ""
+    return ",".join([coords[0].astype(str)] + list(distance.astype(str)))
 
 
 def apply_gmm(csv, bam, out, min_prediction_value=0.99999999, min_number_of_calls=25):
