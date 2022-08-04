7a938b1d779f4511439ff093b550e8d50f383df6
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-08-04 12:46:13 -0700

    try again

--- workflow/scripts/push_m6a_to_bam.py
+++ workflow/scripts/push_m6a_to_bam.py
@@ -30,7 +30,7 @@ def coordinateConversion_MMTag(sequence, base, modification_coords):
     masked_coords = np.cumsum(mask)[coords] - 1
     distance = np.diff(masked_coords) - 1
     if coords.shape[0] < 1:
-        return ""
+        return "", 0
     return (
         ",".join([masked_coords[0].astype(str)] + list(distance.astype(str))),
         coords.shape[0],
