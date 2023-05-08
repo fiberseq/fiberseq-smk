177e04bf9ae96b05939a9976bb3dfbf6eb234c31
Shane J Neph <sjn@n3141.hyak.local>
2023-05-08 12:41:50 -0700

    Update CpG fraction calculation to allow methylation on C or G

--- workflow/scripts/density/cpg-methylation-proportion.sh
+++ workflow/scripts/density/cpg-methylation-proportion.sh
@@ -5,7 +5,7 @@
 set -euox pipefail
 
 if [[ $# != 3 ]]; then
-  printf "Require a reference's cpg locations, fiberseq-smk's all table output, and an output-dir\n"
+  printf "Require a reference's cpg locations, fiberseq-smk's all table output, and an output file name\n"
   printf "Some references are available at /mmfs1/gscratch/stergachislab/sjn/data/cpg/\n"
   exit -1
 fi
@@ -14,19 +14,25 @@ ref_cpg=$1
 exp_cpg=$2
 output=$3
 
+# from Andrew
+#  CpG is CpG on both strands and GpC can be ignored safely.
+#  The main issue in terms of the counting is that the methylation can be at either the C or the G in the CpG dinucleotide.
+
 tmpd=/tmp/`whoami`/$$
-if [[ -d $tmpd ]]; then
-  rm -rf $tmpd
-fi
-mkdir -p $tmpd
+rm -rf ${tmpd}
+mkdir -p ${tmpd}
+
+tmp_ref_cpg=${tmpd}/ref_cpg.plus1bp.bed
+bedops -u --range 0:1 ${ref_cpg} \
+  > ${tmp_ref_cpg}
 
 # use field 4 too, because cpg field gets too big for typical BEDOPS ID field
-hck -z -F \#ct -F st -F en -F fiber -F ref_5mC $exp_cpg \
+hck -z -F \#ct -F st -F en -F fiber -F ref_5mC ${exp_cpg} \
   | awk 'NR > 1 && $2 != $3' \
   | sort-bed - \
-  | tee $tmpd/f \
-  | bedmap --ec --sweep-all --count $ref_cpg - \
-  > $tmpd/g
+  | tee ${tmpd}/f \
+  | bedmap --ec --sweep-all --count ${tmp_ref_cpg} - \
+  > ${tmpd}/g
 
 # ignore last comma
 awk 'BEGIN {OFS="\t"} ; { \
@@ -36,16 +42,17 @@ awk 'BEGIN {OFS="\t"} ; { \
           print $1, a[i], a[i]+1; \
         } \
       } \
-    }' $tmpd/f \
+    }' ${tmpd}/f \
   | sort-bed --max-mem 25G - \
-  | bedmap --faster --sweep-all --exact --delim "\t" --echo --count $ref_cpg - \
-  | paste - $tmpd/g \
+  | bedmap --faster --sweep-all --ec --delim "\t" --echo --count ${tmp_ref_cpg} - \
+  | paste - ${tmpd}/g \
   | awk 'BEGIN {OFS="\t"} ; { \
       if ( $5 > 0 ) { print $1, $2, $3, $4"/"$5, $4/$5; } \
       else { print $1, $2, $3, 0"/"0, 0; } \
     }' \
-  > $output
+  | bedops -u --range 0:-1 - \
+  > ${output}
 
-rm -rf $tmpd
+rm -rf ${tmpd}
 
 exit 0
